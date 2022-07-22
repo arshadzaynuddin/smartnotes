935b3944e56f90fea48399a72eb919e56e7114a3
arshadzaynuddin <arshadzaynuddin@gmail.com>
2022-07-22 19:24:22 +0530

    Updated Host Settings

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--bi854*m=fyxp&$j(d+!zmzbd0x5&-jqx_z#d1jzy*c$$gw1+=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','smartnote-arshuz.herokuapp.com/']
 
 
 # Application definition
